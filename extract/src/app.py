e7559abc0ab3c54a9d7a9a93141165592173d788
abovino <angelo.bovino@outlook.com>
2024-03-15 14:28:54 -0400

    Implement Extract context mgr and send api request.  Modify '__main__' for local testing

--- extract/src/app.py
+++ extract/src/app.py
@@ -1,18 +1,18 @@
 import json
 
+from modules.csvextract import Extract
+
 def extract(event, context):
-    message = event['msg']
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'message': f'Your message: {message}'
-        })
-    }
+    API_RESOURCE_CODE = event['API_RESOURCE_CODE']
+    QUERY_DATE = event['QUERY_DATE']
+    API_TOKEN = event['API_TOKEN']
+    BASE_URL = f'https://data.cityofnewyork.us/resource/{API_RESOURCE_CODE}.csv?'
+    with Extract() as client:
+        data = client.fetch_csv_data(BASE_URL, API_TOKEN, QUERY_DATE)
+
+    return data
 
 if __name__ == '__main__':
-    with open('./extract/events/event.json', 'r', encoding='UTF-8') as f:
+    with open('./extract/events/env.json', 'r', encoding='UTF-8') as f:
         test_event = json.load(f)
         s3_res = extract(test_event, context={})
-        status_code = s3_res['statusCode']
-        msg = json.loads(s3_res['body'])['message']
-        print(f"HTTPStatus: {status_code}, {msg}")
