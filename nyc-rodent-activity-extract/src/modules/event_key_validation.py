02019709b89c602139eb6acff7ace4f7806176d1
Angelo Bovino <angelo.bovino@outlook.com>
2026-03-20 23:56:50 -0400

    Changed file name to match naming conventions and removed some keys

---
+++ nyc-rodent-activity-extract/src/modules/event_key_validation.py
@@ -0,0 +1,33 @@
+"""Module to validate event keys"""
+import re
+import json
+
+def validate_event_keys(event: dict) -> dict:
+    """Checks if mandatory keys exist and if QUERY_DATE is ISO format IE YYYY-MM-DD.
+
+    Args:
+        event (dict): The event dict passed to the AWS Lambda function when it's invoked.
+
+    Returns:
+        dict: Json response with err msg and HTTP status code, or empty dict if no errors.
+    """
+    required_keys = [
+        'API_RESOURCE_CODE', 
+        'QUERY_DATE', 
+        'API_TOKEN',
+        'S3_SUB_DIR',
+    ]
+    for key in required_keys:
+        if key not in event:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': f'Missing event key: {key}'}) 
+            }
+    pattern = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+    match = re.match(pattern, event['QUERY_DATE'])
+    if not match:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "QUERY_DATE format must be YYYY-MM-DD"}),
+        }
+    return {}
